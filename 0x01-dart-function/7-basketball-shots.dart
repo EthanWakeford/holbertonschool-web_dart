12594259c0fefcedcc8efc8b050badc704abf14f
EthanWakeford <ethanwakeford@gmail.com>
2023-10-17 11:27:23 -0500

    task7

--- 0x01-dart-function/7-basketball-shots.dart
+++ 0x01-dart-function/7-basketball-shots.dart
@@ -1,13 +1,13 @@
 int whoWins(Map<String, int> teamA, Map<String, int> teamB) {
   int teamAPoints = 0;
   teamAPoints += 1 * (teamA['Free throws'] ?? 0);
-  teamAPoints += 2 * (teamA['2 pointer'] ?? 0);
-  teamAPoints += 3 * (teamA['3 pointer'] ?? 0);
+  teamAPoints += 2 * (teamA['2 pointers'] ?? 0);
+  teamAPoints += 3 * (teamA['3 pointers'] ?? 0);
 
   int teamBPoints = 0;
   teamBPoints += 1 * (teamB['Free throws'] ?? 0);
-  teamBPoints += 2 * (teamB['2 pointer'] ?? 0);
-  teamBPoints += 3 * (teamB['3 pointer'] ?? 0);
+  teamBPoints += 2 * (teamB['2 pointers'] ?? 0);
+  teamBPoints += 3 * (teamB['3 pointers'] ?? 0);
 
   if (teamAPoints > teamBPoints) {
     return 1;

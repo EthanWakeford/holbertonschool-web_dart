c1912925c618c30c83b4a034a6f20e21655adc7a
EthanWakeford <ethanwakeford@gmail.com>
2023-10-17 16:50:33 -0500

    task 5

---
+++ 0x02-dart-classes/5-json_to_user.dart
@@ -0,0 +1,35 @@
+class User {
+  final String? name;
+  final int? age;
+  final double? height;
+  final int? id;
+
+  User({this.id, this.name, this.age, this.height});
+
+  toJson() {
+    return {'id': id, 'name': name, 'age': age, 'height': height};
+  }
+
+  static User fromJson(Map<dynamic, dynamic> userJson) {
+    return User(
+      id: userJson['id'],
+      name: userJson['name'],
+      age: userJson['age'],
+      height: userJson['height'],
+    );
+  }
+
+  @override
+  String toString() {
+    return 'USER(id: $id, name: $name, age: $age, height: $height)';
+  }
+}
+
+void main() {
+  final djo = User(id: 1, name: "Djo", age: 25, height: 1.89);
+  print(djo.toJson());
+  Map map = {'id': 3, 'name': 'Youssef', 'age': 26, 'height': 1.9};
+  final youssef = User.fromJson(map);
+  print(djo.toString());
+  print(youssef.toString());
+}

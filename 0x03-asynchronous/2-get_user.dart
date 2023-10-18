fa7eee4a807c9d77b1e3879314c3512c2272d438
EthanWakeford <ethanwakeford@gmail.com>
2023-10-18 12:07:42 -0500

    task 2

---
+++ 0x03-asynchronous/2-get_user.dart
@@ -0,0 +1,13 @@
+import '2-util.dart';
+
+Future<void> getUser() async {
+  try {
+    await fetchUser();
+  } catch (err) {
+    print('error caught: $err');
+  }
+}
+
+main() async {
+  getUser();
+}

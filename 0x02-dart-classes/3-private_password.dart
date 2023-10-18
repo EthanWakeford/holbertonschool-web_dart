5b13f0c2c6b369287bb4e18821cb0554bf6b9828
EthanWakeford <ethanwakeford@gmail.com>
2023-10-18 10:35:52 -0500

    task 3

--- 0x02-dart-classes/3-private_password.dart
+++ 0x02-dart-classes/3-private_password.dart
@@ -6,6 +6,10 @@ class Password {
     return 'Your Password is: $_password';
   }
 
+  Password({required String password}) {
+    _password = password;
+  }
+
   bool isValid() {
     if (_password.length < 8 || _password.length > 16) return false;
     if (!_password.contains(RegExp(r'[a-z]'))) return false;
@@ -13,4 +17,13 @@ class Password {
     if (!_password.contains(RegExp(r'[0-9]'))) return false;
     return true;
   }
+}
+
+void main() {
+  final ps = Password(password: "Passwordcode");
+  print(ps.toString());
+  print(ps.isValid());
+  final ps2 = Password(password: "PasswordDecode3");
+  print(ps2.toString());
+  print(ps2.isValid());
 }

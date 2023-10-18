174cd6d70c5a1bca6dc53b46a84618facca72b32
EthanWakeford <ethanwakeford@gmail.com>
2023-10-18 10:37:16 -0500

    task 5

---
+++ 0x02-dart-classes/6-inheritance.dart
@@ -0,0 +1,37 @@
+class Password {
+  String _password = '';
+
+  Password({required String password}) {
+    _password = password;
+  }
+
+  String get password {
+    return _password;
+  }
+
+  void set password(String password) {
+    _password = password;
+  }
+
+  @override
+  String toString() {
+    return 'Your Password is: $_password';
+  }
+
+  bool isValid() {
+    if (_password.length < 8 || _password.length > 16) return false;
+    if (!_password.contains(RegExp(r'[a-z]'))) return false;
+    if (!_password.contains(RegExp(r'[A-Z]'))) return false;
+    if (!_password.contains(RegExp(r'[0-9]'))) return false;
+    return true;
+  }
+}
+
+void main() {
+  final password = Password(password: "Passwordecode");
+  print(password.isValid());
+  print(password.toString());
+  password.password = "Youssef4321";
+  print(password.isValid());
+  print(password.toString());
+}

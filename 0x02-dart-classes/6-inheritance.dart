c278e4718b17158e7c0aa3743ed0f95d845f73fe
EthanWakeford <ethanwakeford@gmail.com>
2023-10-18 11:27:02 -0500

    task 6

--- 0x02-dart-classes/6-inheritance.dart
+++ 0x02-dart-classes/6-inheritance.dart
@@ -1,37 +1,68 @@
-class Password {
-  String _password = '';
+import '6-password.dart';
 
-  Password({required String password}) {
-    _password = password;
+class User extends Password {
+  final String? name;
+  final int? age;
+  final double? height;
+  final int? id;
+
+  User({
+    this.id,
+    this.name,
+    this.age,
+    this.height,
+    required String user_password,
+  }) : super(password: user_password) {}
+
+  String get user_password => password;
+
+  set user_password(String value) {
+    password = value;
   }
 
-  String get password {
-    return _password;
+  toJson() {
+    return {'id': id, 'name': name, 'age': age, 'height': height};
   }
 
-  void set password(String password) {
-    _password = password;
+  static User fromJson(Map<dynamic, dynamic> userJson) {
+    return User(
+        id: userJson['id'],
+        name: userJson['name'],
+        age: userJson['age'],
+        height: userJson['height'],
+        user_password: userJson['user_password']);
   }
 
   @override
   String toString() {
-    return 'Your Password is: $_password';
-  }
-
-  bool isValid() {
-    if (_password.length < 8 || _password.length > 16) return false;
-    if (!_password.contains(RegExp(r'[a-z]'))) return false;
-    if (!_password.contains(RegExp(r'[A-Z]'))) return false;
-    if (!_password.contains(RegExp(r'[0-9]'))) return false;
-    return true;
+    return 'User(id : $id ,name: $name, age: $age, height: $height, Password: ${this.isValid()})';
   }
 }
 
 void main() {
-  final password = Password(password: "Passwordecode");
-  print(password.isValid());
-  print(password.toString());
-  password.password = "Youssef4321";
-  print(password.isValid());
-  print(password.toString());
+  final djo = User(
+      id: 1, name: "Djo", age: 25, height: 1.89, user_password: "Azert23defde");
+  print('<===========Json=========>:');
+  print('\n');
+  print(djo.toJson());
+  Map map = {
+    'id': 3,
+    'name': 'Youssef',
+    'age': 26,
+    'height': 1.9,
+    'user_password': "Azert23defde"
+  };
+  final youssef = User.fromJson(map);
+  print('\n');
+  print('<===========Test1===========>:');
+  print('\n');
+  print(djo.toString());
+  print(youssef.toString());
+  print('\n');
+  print('<===========Test2===========>:');
+  print('\n');
+  djo.user_password = "holberton98";
+  youssef.user_password = "AZERfghn6789";
+  print(djo.toString());
+  print(youssef.toString());
 }
--- 0x02-dart-classes/6-password.dart
+++ 0x02-dart-classes/6-password.dart
@@ -1,1 +1,28 @@
+class Password {
+  String _password = '';
 
+  Password({required String password}) {
+    _password = password;
+  }
+
+  String get password {
+    return _password;
+  }
+
+  void set password(String password) {
+    _password = password;
+  }
+
+  @override
+  String toString() {
+    return 'Your Password is: $_password';
+  }
+
+  bool isValid() {
+    if (_password.length < 8 || _password.length > 16) return false;
+    if (!_password.contains(RegExp(r'[a-z]'))) return false;
+    if (!_password.contains(RegExp(r'[A-Z]'))) return false;
+    if (!_password.contains(RegExp(r'[0-9]'))) return false;
+    return true;
+  }
+}
